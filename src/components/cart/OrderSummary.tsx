230262676a31784624765a07ed0c97c16785bd5e
devsrsihab <srsohankhan0@gmail.com>
2024-07-12 19:17:32 +0600

    cart  page design remove dark classes

--- src/components/cart/OrderSummary.tsx
+++ src/components/cart/OrderSummary.tsx
@@ -1,67 +1,48 @@
 const OrderSummary = () => {
   return (
     <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
-      <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
-        <p className="text-xl font-semibold text-gray-900 dark:text-white">
-          Order summary
-        </p>
+      <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6">
+        <p className="text-xl font-semibold text-gray-900">Order summary</p>
         <div className="space-y-4">
           <div className="space-y-2">
             <dl className="flex items-center justify-between gap-4">
-              <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+              <dt className="text-base font-normal text-gray-500">
                 Original price
               </dt>
-              <dd className="text-base font-medium text-gray-900 dark:text-white">
-                $7,592.00
-              </dd>
+              <dd className="text-base font-medium text-gray-900">$7,592.00</dd>
             </dl>
             <dl className="flex items-center justify-between gap-4">
-              <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                Savings
-              </dt>
+              <dt className="text-base font-normal text-gray-500">Savings</dt>
               <dd className="text-base font-medium text-green-600">-$299.00</dd>
             </dl>
             <dl className="flex items-center justify-between gap-4">
-              <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
+              <dt className="text-base font-normal text-gray-500">
                 Store Pickup
               </dt>
-              <dd className="text-base font-medium text-gray-900 dark:text-white">
-                $99
-              </dd>
+              <dd className="text-base font-medium text-gray-900">$99</dd>
             </dl>
             <dl className="flex items-center justify-between gap-4">
-              <dt className="text-base font-normal text-gray-500 dark:text-gray-400">
-                Tax
-              </dt>
-              <dd className="text-base font-medium text-gray-900 dark:text-white">
-                $799
-              </dd>
+              <dt className="text-base font-normal text-gray-500">Tax</dt>
+              <dd className="text-base font-medium text-gray-900">$799</dd>
             </dl>
           </div>
-          <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2 dark:border-gray-700">
-            <dt className="text-base font-bold text-gray-900 dark:text-white">
-              Total
-            </dt>
-            <dd className="text-base font-bold text-gray-900 dark:text-white">
-              $8,191.00
-            </dd>
+          <dl className="flex items-center justify-between gap-4 border-t border-gray-200 pt-2">
+            <dt className="text-base font-bold text-gray-900">Total</dt>
+            <dd className="text-base font-bold text-gray-900">$8,191.00</dd>
           </dl>
         </div>
         <a
           href="#"
-          className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300"
         >
           Proceed to Checkout
         </a>
         <div className="flex items-center justify-center gap-2">
-          <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-            {" "}
-            or{" "}
-          </span>
+          <span className="text-sm font-normal text-gray-500"> or </span>
           <a
             href="#"
             title=""
-            className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline dark:text-primary-500"
+            className="inline-flex items-center gap-2 text-sm font-medium text-primary-700 underline hover:no-underline"
           >
             Continue Shopping
             <svg
@@ -82,27 +63,26 @@ const OrderSummary = () => {
           </a>
         </div>
       </div>
-      <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
+      <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm sm:p-6">
         <form className="space-y-4">
           <div>
             <label
               htmlFor="voucher"
-              className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              className="mb-2 block text-sm font-medium text-gray-900"
             >
-              {" "}
-              Do you have a voucher or gift card?{" "}
+              Do you have a voucher or gift card?
             </label>
             <input
               type="text"
               id="voucher"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500"
               placeholder=""
               required={true}
             />
           </div>
           <button
             type="submit"
-            className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300"
           >
             Apply Code
           </button>
--- src/pages/CartPage .tsx
+++ src/pages/CartPage .tsx
@@ -3,10 +3,10 @@ import OrderSummary from "../components/cart/OrderSummary";
 
 const CartPage = () => {
   return (
-    <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+    <section className="bg-white py-8 shadow-lg antialiased md:py-16">
       <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
-        <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
-          Shopping CartPage
+        <h2 className="text-xl font-semibold text-gray-900 sm:text-2xl">
+          Shopping Cart
         </h2>
         <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
           <CartList />
